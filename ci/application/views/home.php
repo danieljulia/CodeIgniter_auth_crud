611e6a522c0c52c6577206ee40d5ceae332d742c
Daniel Julià @daniel_julia <dani@pimpampum.net>
2014-10-23 20:02:56 +0200

    code igniter

---
+++ ci/application/views/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+
+
+</head>
+<body>
+
+<h1>CodeIgniter + Ion Auth + GRocery CRUD</h1>
+<ul>
+<li><a href="<?php echo site_url("welcome");?>">CI home</a></li>
+<li><a href="<?php echo site_url("auth/login");?>">Auth login</a> (admin@admin.com / password)</li>
+<li><a href="<?php echo site_url("examples");?>">CRUD sample</a></li>
+</ul>
+</body>
+</html>
